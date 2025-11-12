024d7c219b0f4b0c650752e68f35af579e2568d9
Aayushi Tewari <111345830+faddednatasha@users.noreply.github.com>
2025-11-12 23:22:14 +0530

    Sum of 2 numbers

---
+++ 2.py
@@ -0,0 +1,14 @@
+#Program No. : 2
+#Program : Calculate sum of 2 numbers.
+
+# Define the two numbers
+number1 = 10
+number2 = 5
+
+# Calculate the sum
+sum_of_numbers = number1 + number2
+
+# Display the result
+print(f"The first number is: {number1}")
+print(f"The second number is: {number2}")
+print(f"The sum of {number1} and {number2} is: {sum_of_numbers}")
