5cddc63e26dda642113d27d0c6f45566a6576416
Aayushi Tewari <111345830+faddednatasha@users.noreply.github.com>
2025-11-13 11:24:57 +0530

    Division of numbers

---
+++ 5.py
@@ -0,0 +1,14 @@
+Program No. : 5
+#Program : Calculate division of numbers.
+
+# Define the two numbers
+numerator = 10
+denominator = 5
+
+# Calculate the quotient
+quotient_of_numbers = numerator / denominator
+
+# Display the result
+print(f"The first number is: {numerator}")
+print(f"The second number is: {denominator}")
+print(f"The quotient of {numerator} and {denominator} is: {quotient_of_numbers}")
