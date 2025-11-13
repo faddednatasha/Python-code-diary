3ae6e2dc5de39dac4c177cc06ead65c0a5d284f7
Aayushi Tewari <111345830+faddednatasha@users.noreply.github.com>
2025-11-13 11:09:05 +0530

    Difference of 2 number

---
+++ 3.py
@@ -0,0 +1,14 @@
+Program No. : 3
+#Program : Calculate difference of 2 numbers.
+
+# Define the two numbers
+number1 = 10
+number2 = 5
+
+# Calculate the difference
+difference_of_numbers = number1 - number2
+
+# Display the result
+print(f"The first number is: {number1}")
+print(f"The second number is: {number2}")
+print(f"The difference of {number1} and {number2} is: {difference_of_numbers}")
