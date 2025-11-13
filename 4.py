7e47cf54c30ea05c941c14672c934c4bb5f1b25e
Aayushi Tewari <111345830+faddednatasha@users.noreply.github.com>
2025-11-13 11:15:55 +0530

    Multiplication of numbers

---
+++ 4.py
@@ -0,0 +1,14 @@
+Program No. : 4
+#Program : Calculate multiplication of numbers.
+
+# Define the two numbers
+number1 = 10
+number2 = 5
+
+# Calculate the product
+multiplication_of_numbers = number1 * number2
+
+# Display the result
+print(f"The first number is: {number1}")
+print(f"The second number is: {number2}")
+print(f"The product of {number1} and {number2} is: {multiplication_of_numbers}")
